22461da6aec91a8c1f745725cb83f0df61a29ea1
SarvinTorkaman <88194904+SarvinTorkaman@users.noreply.github.com>
2021-10-23 17:55:42 +0330

    made it responsive

--- javascript.js
+++ javascript.js
@@ -1,23 +1,27 @@
 function handleResponse(response) {
-  console.log(response);
-  let desplayResult = document.querySelector("#display-result");
+  if (typeof response === "object") {
+    console.log(response);
+    let desplayResult = document.querySelector("#display-result");
 
-  let resultHtml = ` <h2>${response.data[0].word}</h2>`;
+    let resultHtml = ` <h2>${response.data[0].word}</h2>`;
 
-  response.data[0].meanings.forEach((element) => {
-    console.log(element);
-    // console.log("hello");
-    resultHtml = resultHtml + `<h3>${element.partOfSpeech}</h3>`;
-    element.definitions.forEach((element) => {
-      resultHtml = resultHtml + `<p>${element.definition}</p> `;
+    response.data[0].meanings.forEach((element) => {
+      console.log(element);
+      // console.log("hello");
+      resultHtml = resultHtml + `<h3>${element.partOfSpeech}</h3>`;
+      element.definitions.forEach((element) => {
+        resultHtml = resultHtml + `<p>${element.definition}</p> `;
 
-      if (element.example !== undefined) {
-        resultHtml = resultHtml + `<div> <em>${element.example}</em> </div>`;
-      }
+        if (element.example !== undefined) {
+          resultHtml = resultHtml + `<div> <em>${element.example}</em> </div>`;
+        }
+      });
+      resultHtml = resultHtml + `<hr>`;
+      desplayResult.innerHTML = resultHtml;
     });
-    resultHtml = resultHtml + `<hr>`;
-    desplayResult.innerHTML = resultHtml;
-  });
+  } else {
+    alert("No result for that word");
+  }
 }
 
 function handlesubmit(event) {
@@ -27,6 +31,9 @@ function handlesubmit(event) {
   let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${word}`;
 
   axios.get(apiUrl).then(handleResponse);
+  axios.get(apiUrl).catch(function (error) {
+    alert(`${word} is not found in english dictionary`);
+  });
 }
 let dictionaryForm = document.querySelector("#dictionar-form");
 dictionaryForm.addEventListener("submit", handlesubmit);
